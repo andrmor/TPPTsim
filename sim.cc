8f4c823bbbdc996e843e381ac697a193ea31d555
Andr <andreimorozov@hotmail.com>
2021-07-22 19:15:50 +0100

    ++

--- sim.cc
+++ sim.cc
@@ -25,7 +25,7 @@ int main(int argc, char** argv)
     // warning: automatically saves config in working directory as SimConfig.json
     // beware of possible overright!
     //here you can directly provide the config file name
-    filename = "/home/andr/WORK/TPPT/SimConfig1.json";
+    //filename = "/home/andr/WORK/TPPT/SimConfig1.json";
 
     if (!filename.empty())
     {
