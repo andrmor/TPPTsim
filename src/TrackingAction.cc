fe3448bc0c0e6b072e5f3ca547ace0e0f3751f18
Andrey Morozov <andreimorozov@hotmail.com>
2022-10-13 11:37:32 +0100

    ++

--- src/TrackingAction.cc
+++ src/TrackingAction.cc
@@ -6,6 +6,7 @@
 
 #include "G4Track.hh"
 #include "G4Positron.hh"
+#include "G4VProcess.hh"
 
 void PesGeneratorTrackingAction::PreUserTrackingAction(const G4Track * /*track*/)
 {
@@ -19,6 +20,10 @@ void AnnihilationLoggerTrackingAction::PostUserTrackingAction(const G4Track * tr
     if (track->GetParticleDefinition() != G4Positron::Definition()) return;
 
     //out(track->GetPosition()[0], track->GetPosition()[1], track->GetPosition()[2]);
+
+    //const G4VProcess * proc = track->GetStep()->GetPostStepPoint()->GetProcessDefinedStep();
+    //std::string procName = (proc ? proc->GetProcessName() : "undefined");
+    //if (procName != "annihil") out(procName);
 
     SessionManager & SM = SessionManager::getInstance();
     AnnihilationLoggerMode * mode = static_cast<AnnihilationLoggerMode*>(SM.SimMode);
